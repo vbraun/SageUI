f41c51a5e2fbbee9702c17d1d8a4a0a17573bca1
Volker Braun <vbraun.name@gmail.com>
2013-08-09 17:26:30 +0100

    ipython integration

--- src/sageui/cmdline.py
+++ src/sageui/cmdline.py
@@ -8,32 +8,35 @@ import os
 
 def check_gui_prerequisites():
     try:
+        import gtk
+    except:
+        print 'Fatal: You need the Python GTK interface!'
+        sys.exit(1)
+    try:
         import pygtk
         pygtk.require('2.0')
     except:
-        print 'You need PyGTK version 2 or higher!'
+        print 'Fatal: You need PyGTK version 2 or higher!'
         sys.exit(1)
     try:
-        import gtk
         import gtk.glade
     except:
-        print 'You need the Python Glade interface!'
+        print 'Fatal: You need the Python Glade interface!'
         sys.exit(1)
     try:
         import cairo
     except:
-        print 'You need the Python Cairo interface!'
+        print 'Fatal: You need the Python Cairo interface!'
         sys.exit(1)
     try: 
         import vte
     except:
-        print 'You need VTE!'
+        print 'Fatal: You need VTE!'
         sys.exit(1)
 
 
 def launch_gui(debug=False):
     check_gui_prerequisites()
-    import gtk
     from sageui.gui.app import Application
     app = Application()
     import sageui
@@ -41,6 +44,12 @@ def launch_gui(debug=False):
     if debug:
         from IPython.lib.inputhook import enable_gtk
         enable_gtk()
+        from IPython.frontend.terminal.ipapp import TerminalIPythonApp
+        ip = TerminalIPythonApp.instance()
+        ip.initialize(argv=[])
+        ip.shell.enable_gui('gtk')
+        ip.shell.user_global_ns['app'] = app
+        ip.start()
     else:
         gtk.main()
 
@@ -54,13 +63,10 @@ description = """
 def launch():
     from argparse import ArgumentParser
     parser = ArgumentParser(description=description)
-    parser.add_argument('--gui', dest='gui', action='store_true',
-                        default=True, 
-                        help='start the gui')
-    parser.add_argument('--debug', dest='debug', action='store_false',
+    parser.add_argument('--debug', dest='debug', action='store_true',
                         default=False, 
                         help='debug')
     args = parser.parse_args()
-    
+    print args
     if args.gui:
         launch_gui(debug=args.debug)
