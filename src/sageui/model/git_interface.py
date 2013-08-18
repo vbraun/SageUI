42c3f7ae98ee5efa8ffd34fc24741d3de78954e4
Volker Braun <vbraun.name@gmail.com>
2013-08-18 14:12:09 +0100

    GPLv3 copyright

--- src/sageui/model/git_interface.py
+++ src/sageui/model/git_interface.py
@@ -26,17 +26,31 @@ EXAMPLES::
     DEBUG cmd: git ls-files --exclude-standard --other
     DEBUG stdout: untracked
     ['untracked']
-
-   
 """
-#*****************************************************************************
-#       Copyright (C) 2013 TODO
+
+##############################################################################
+#  SageUI: A graphical user interface to Sage, Trac, and Git.
+#  Copyright (C) 2013  Volker Braun <vbraun.name@gmail.com>
+#                      David Roe <roed.math@gmail.com>
+#                      Julian Rueth <julian.rueth@fsfe.org>
+#                      Keshav Kini <keshav.kini@gmail.com>
+#                      Nicolas M. Thiery <Nicolas.Thiery@u-psud.fr>
+#                      Robert Bradshaw <robertwb@gmail.com>
 #
-#  Distributed under the terms of the GNU General Public License (GPL)
-#  as published by the Free Software Foundation; either version 2 of
-#  the License, or (at your option) any later version.
-#                  http://www.gnu.org/licenses/
-#*****************************************************************************
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+##############################################################################
+
 
 import os
 import subprocess
--- src/sageui/presenter.py
+++ src/sageui/presenter.py
@@ -4,9 +4,26 @@ The Presenter (Controller)
 Here is where it all comes together, the presenter ties together the
 data model with the gui to create an application. It neither knows
 about data nor about the gui, it just ties the two together.
-
-
 """
+
+##############################################################################
+#  SageUI: A graphical user interface to Sage, Trac, and Git.
+#  Copyright (C) 2013  Volker Braun <vbraun.name@gmail.com>
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+##############################################################################
+
 
 from model.trac_error import TracError
 
